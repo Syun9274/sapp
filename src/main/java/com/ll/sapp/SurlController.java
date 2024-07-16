98d16200b92a7b2e1c6e5fb25ec635f5af08ceda
Syun9274 <zxczxv7624@gmail.com>
2024-07-16 11:25:01 +0900

    3 - 11

--- src/main/java/com/ll/sapp/SurlController.java
+++ src/main/java/com/ll/sapp/SurlController.java
@@ -1,10 +1,13 @@
 package com.ll.sapp;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -26,4 +29,30 @@ public class SurlController {
 
         return surl;
     }
+
+    @GetMapping("/s/{body}/**")
+    @ResponseBody
+    public Surl add(@PathVariable String body, HttpServletRequest request) {
+        String url = request.getRequestURI();
+
+        if(request.getQueryString() != null) {
+            url += '?' + request.getQueryString();
+        }
+
+        String[] urlBits = url.split("/", 4);
+        System.out.println("Arrays.toString(urlBits) : " + Arrays.toString(urlBits));
+
+        url = urlBits[3];
+
+        Surl surl = Surl.builder()
+                .id(++surlsLastId)
+                .body(body)
+                .url(url)
+                .build();
+
+        surls.add(surl);
+
+        return surl;
+    }
+
 }
