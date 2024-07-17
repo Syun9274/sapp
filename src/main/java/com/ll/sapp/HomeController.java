a6c79e9116746e2ff8221ef49dfb4d4eddc9d67d
Syun9274 <zxczxv7624@gmail.com>
2024-07-17 14:24:31 +0900

    hotfix

--- src/main/java/com/ll/sapp/HomeController.java
+++ src/main/java/com/ll/sapp/HomeController.java
@@ -11,12 +11,9 @@ public class HomeController {
     @Value("${custom.site.name}")
     private String siteName;
 
-    @Value("${custom.secret.key}")
-    private String secretKey;
-
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "Hello, " + siteName + secretKey;
+        return "Hello, " + siteName;
     }
 }
