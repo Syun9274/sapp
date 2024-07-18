7de24acadb5391b881e30821c7f2e584bf368686
Syun9274 <zxczxv7624@gmail.com>
2024-07-18 13:46:42 +0900

    6 - 12

--- src/main/java/com/ll/sapp/domain/article/article/entity/Article.java
+++ src/main/java/com/ll/sapp/domain/article/article/entity/Article.java
@@ -2,6 +2,11 @@ package com.ll.sapp.domain.article.article.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Builder
@@ -9,11 +14,18 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdTime;
+
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
 
     @Column
     private String title;
--- src/main/java/com/ll/sapp/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sapp/domain/article/article/repository/ArticleRepository.java
@@ -4,4 +4,5 @@ import com.ll.sapp.domain.article.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
 }
